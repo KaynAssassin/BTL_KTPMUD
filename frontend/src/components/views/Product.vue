f2169b762869749f214afb1df6044e91ce9d8230
Thiều Anh Nguyên <nguyenta.hit@gmail.com>
2019-12-10 14:10:09 +0700

    ...

--- frontend/src/components/views/Product.vue
+++ frontend/src/components/views/Product.vue
@@ -2,7 +2,9 @@
   <div class="docente">
     <div class="formodal">
       <v-dialog v-model="dialog" max-width="800px">
-        <v-btn color="primary" dark slot="activator" class="mb-2">Thêm sản phẩm</v-btn>
+        <v-btn color="primary" dark slot="activator" class="mb-2"
+          >Thêm sản phẩm</v-btn
+        >
         <v-card>
           <v-card-title>
             <span class="headline">{{ formTitle }}</span>
@@ -11,23 +13,43 @@
             <v-container grid-list-md>
               <v-layout wrap>
                 <v-flex xs12 sm6 md4>
-                  <v-text-field label="Nombres" v-model="editedItem.name"></v-text-field>
+                  <v-text-field
+                    label="Tên sản phẩm"
+                    v-model="editedItem.product_name"
+                  ></v-text-field>
                 </v-flex>
                 <v-flex xs12 sm6 md4>
-                  <v-text-field label="Apellidos" v-model="editedItem.apellidos"></v-text-field>
+                  <v-text-field
+                    label="Mã thương hiệu"
+                    v-model="editedItem.brand_id"
+                  ></v-text-field>
                 </v-flex>
                 <v-flex xs12 sm6 md4>
-                  <v-text-field label="edad" v-model="editedItem.edad"></v-text-field>
+                  <v-text-field
+                    label="Mã chủng loại"
+                    v-model="editedItem.category_id"
+                  ></v-text-field>
                 </v-flex>
                 <v-flex xs12 sm6 md4>
-                  <v-text-field label="email" v-model="editedItem.email"></v-text-field>
+                  <v-text-field
+                    label="Ngày tạo"
+                    v-model="editedItem.date_import"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field
+                    label="Giá"
+                    v-model="editedItem.price"
+                  ></v-text-field>
                 </v-flex>
               </v-layout>
             </v-container>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+            <v-btn color="blue darken-1" flat @click.native="close"
+              >Cancel</v-btn
+            >
             <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
           </v-card-actions>
         </v-card>
@@ -62,125 +84,125 @@
 </template>
 
 <script>
-import apiService from '@/Services/ApiService'
+import apiService from "@/Services/ApiService";
 export default {
-  name: 'Product',
-  data () {
+  name: "Product",
+  data() {
     return {
       docentes: [],
       dialog: false,
       editedIndex: -1,
       editedItem: {
-        iddocente: '',
-        name: '',
-        apellidos: '',
-        edad: '',
-        email: ''
+        product_name: '',
+        brand_id: '',
+        category_id: '',
+        date_import: '',
+        price: ''
       },
       headers: [
         {
-          text: 'UID Docente',
-          align: 'left',
+          text: "UID Docente",
+          align: "left",
           sortable: true,
-          value: 'iddocente'
+          value: "iddocente"
         },
         {
-          text: 'Nombres',
-          align: 'left',
+          text: "Nombres",
+          align: "left",
           sortable: true,
-          value: 'name'
+          value: "name"
         },
         {
-          text: 'Apellidos',
-          align: 'left',
+          text: "Apellidos",
+          align: "left",
           sortable: false,
-          value: 'apellidos'
+          value: "apellidos"
         },
         {
-          text: 'Edad',
-          align: 'left',
+          text: "Edad",
+          align: "left",
           sortable: false,
-          value: 'edad'
+          value: "edad"
         },
         {
-          text: 'E-mail',
-          align: 'left',
+          text: "E-mail",
+          align: "left",
           sortable: false,
-          value: 'email'
+          value: "email"
         },
-        { text: 'Actions', value: 'name', sortable: false }
+        { text: "Actions", value: "name", sortable: false }
       ]
-    }
+    };
   },
-  mounted () {
-    this.getDocente()
+  mounted() {
+    this.getDocente();
   },
   computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
     }
   },
   watch: {
-    dialog (val) {
-      val || this.close()
+    dialog(val) {
+      val || this.close();
     }
   },
   methods: {
-    async getDocente () {
-      const response = await apiService.fetchDocentes()
-      this.docentes = response.data
+    async getDocente() {
+      const response = await apiService.fetchDocentes();
+      this.docentes = response.data;
     },
 
-    async save () {
+    async save() {
       try {
         if (this.editedIndex === -1) {
-          await apiService.addDocente(this.editedItem)
-          this.docentes.push(this.editedItem)
+          await apiService.addDocente(this.editedItem);
+          this.docentes.push(this.editedItem);
         } else {
-          await apiService.updateDocente(this.editedItem)
-          Object.assign(this.docentes[this.editedIndex], this.editedItem)
+          await apiService.updateDocente(this.editedItem);
+          Object.assign(this.docentes[this.editedIndex], this.editedItem);
         }
       } catch (err) {
-        return console.log(err.message)
+        return console.log(err.message);
       } finally {
-        this.close()
+        this.close();
       }
     },
 
-    async deleteDocente (docente) {
-      const $this = this
-      $this.$swal({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
-        type: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Yes, delete it!'
-      }).then((result) => {
-        if (result.value) {
-          apiService.deleteDocente(docente.iddocente)
-          const index = this.docentes.indexOf(docente)
-          this.docentes.splice(index, 1)
-        }
-      })
+    async deleteDocente(docente) {
+      const $this = this;
+      $this
+        .$swal({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Yes, delete it!"
+        })
+        .then(result => {
+          if (result.value) {
+            apiService.deleteDocente(docente.iddocente);
+            const index = this.docentes.indexOf(docente);
+            this.docentes.splice(index, 1);
+          }
+        });
     },
 
-    editItem (item) {
-      this.editedIndex = this.docentes.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
+    editItem(item) {
+      this.editedIndex = this.docentes.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
     },
 
-    close () {
-      this.dialog = false
+    close() {
+      this.dialog = false;
       setTimeout(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      }, 300)
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
     }
   }
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
